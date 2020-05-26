ca45ee4b4d583b5dc214ae66dc7f11bc6da6c1f2
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-26 23:22:30 +0200

    Brew formula update for kin version v1.0.8

--- kin.rb
+++ kin.rb
@@ -2,16 +2,16 @@
 class Kin < Formula
   desc "Automatic home page of self hosted dockerized web application"
   homepage ""
-  version "1.0.6"
+  version "1.0.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/marema31/kin/releases/download/v1.0.6/kin_1.0.6_Darwin_x86_64.tar.gz"
-    sha256 "d003f4667e4ad87f886966618471b18e21af5d09bb4807f9d9a397b2496cf866"
+    url "https://github.com/marema31/kin/releases/download/v1.0.8/kin_1.0.8_Darwin_x86_64.tar.gz"
+    sha256 "f8f3ef9407febd971f82e9d9f15349cdd88637541be2b136e5b1ee4c76decdf2"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/marema31/kin/releases/download/v1.0.6/kin_1.0.6_Linux_x86_64.tar.gz"
-      sha256 "be947aa8038062830a9346ca2f4bb50c1598749f1c730b38600dfcfb89a72308"
+      url "https://github.com/marema31/kin/releases/download/v1.0.8/kin_1.0.8_Linux_x86_64.tar.gz"
+      sha256 "00728fe4c92754ec4b2002b68af4c68ccfd52e9f30f37a9f27524d49e8f27115"
     end
   end
 
