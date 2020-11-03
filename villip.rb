45ac97fb468931f11f25ce70b0e3d4951f2baf9b
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-03 13:56:02 +0100

    Brew formula update for villip version v1.1.2

--- villip.rb
+++ villip.rb
@@ -2,16 +2,16 @@
 class Villip < Formula
   desc "Filtering proxy for development environment"
   homepage ""
-  version "1.1.1"
+  version "1.1.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/marema31/villip/releases/download/v1.1.1/villip_1.1.1_Darwin_x86_64.tar.gz"
-    sha256 "e4ddb4a30ed58ff47693b6cd2809ebe689227c3255f793ecbfa3dd21e81de15c"
+    url "https://github.com/marema31/villip/releases/download/v1.1.2/villip_1.1.2_Darwin_x86_64.tar.gz"
+    sha256 "018c22c2c5ebeb01f2397df8dd6e93a8a35ff34f80a8af8b9f787cfed723b87f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/marema31/villip/releases/download/v1.1.1/villip_1.1.1_Linux_x86_64.tar.gz"
-      sha256 "f84ed28e4fae295f9a50ab3d32afd58890b0269f8c7a4bd6f5a6fb47f6c0f9ca"
+      url "https://github.com/marema31/villip/releases/download/v1.1.2/villip_1.1.2_Linux_x86_64.tar.gz"
+      sha256 "c99e70466f94aba73924b1e59853a023d8853474a98a569b7b9455df800895d5"
     end
   end
 
